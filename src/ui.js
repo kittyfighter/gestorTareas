c4271627b11329a5ab67e5820021118ab7226ec7
Ibeth <stephanieibeth@gmail.com>
2024-08-09 00:50:39 -0600

    Estilos CSS, función agregar tareas, función traer tareas y función para visualizar tareas, UI.js

---
+++ src/ui.js
@@ -0,0 +1,26 @@
+// Importar la función para traer las tareas del LocalStorage
+import { getTasks } from "./task";
+
+// Función para visualizar las tareas
+export const renderTasks = () => {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+    const tasks = getTasks();
+    tasks.forEach((task) => {
+        const li = document.createElement("li");
+        li.setAttribute("data-id", task.id);
+
+        if(task.completed === true) {
+            li.classList.add("completed");
+        }
+
+        li.innerHTML = `
+            ${task.text}
+            <button class="delete"> Borrar </button>
+            <button class="toggle"> ${task.completed === true ? "Regresar" : "Completado" } </button>
+        `;
+
+        taskList.appendChild(li);
+    });
+};
+
